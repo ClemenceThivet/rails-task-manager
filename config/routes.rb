d125e77e6fb0aba85af794f9db4901f2f85020d9
Clemence Thivet <clemence.thivet@gmail.com>
2016-11-15 16:51:02 +0100

    ok ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # Get all tasks
+  # Get one tasks
+  # Get the form to create a new task
+  # Post a new task
+  # Get the form to edit a existing task
+  # update an existing task
+  # delete an existing task
 end
